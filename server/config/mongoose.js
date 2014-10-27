1001dfed1f6623b6f0e4b4cbbab65118da68e3e3
jatjo <winterland78@hotmail.com>
2014-10-27 18:39:13 +0100

    Added passport module and started creating simple login.

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,20 @@ module.exports = function(config) {
     db.once('open', function callback() {
         console.log('kitstash db opened');
     });
+
+    var userSchema = mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        userName: String
+    });
+    var User = mongoose.model('User', userSchema);
+
+    User.find({}).exec(function(err, collection){
+        if (collection.length === 0) {
+            User.create({firstName:'Jan Terje', lastName:'Johansen', userName:'jatjo'});
+            User.create({firstName:'Linda', lastName:'Fahle-Johansen', userName:'linda'});
+            User.create({firstName:'Erik', lastName:'Fahle-Johansen', userName:'erik'});
+            User.create({firstName:'Eskil', lastName:'Fahle-Johansen', userName:'eskil'});
+        }
+    })
 }
