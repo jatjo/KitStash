80df0087cd8fb32f0802ac267641ca6d28442503
jatjo <winterland78@hotmail.com>
2014-10-31 21:20:40 +0100

    Implemented validation.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -9,11 +9,12 @@ exports.getUsers = function(req, res) {
 
 exports.createUser = function(req, res, next) {
     var userData = req.body;
+    userData.username = userData.username.toLowerCase();
     userData.salt = encrypt.createSalt();
     userData.hashed_pwd = encrypt.hashPwd(userData.salt, userData.password);
     User.create(userData, function(err, user) {
         if(err) {
-            if(err.toString().indexOf('E110000') > -1) {
+            if(err.toString().indexOf('E11000') > -1) {
                 err = new Error('Duplicate Username');
             }
             res.status(400);
