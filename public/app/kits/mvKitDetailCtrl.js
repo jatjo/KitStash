754fb0337cbff36780bb8e30ed859bde57202dcc
jatjo <winterland78@hotmail.com>
2014-11-01 23:03:00 +0100

    Added caching of single kit items.

--- public/app/kits/mvKitDetailCtrl.js
+++ public/app/kits/mvKitDetailCtrl.js
@@ -1,3 +1,9 @@
-angular.module('app').controller('mvKitDetailCtrl', function($scope, $routeParams, mvKit) {
-    $scope.kit = mvKit.get({_id:$routeParams.id})
+angular.module('app').controller('mvKitDetailCtrl', function($scope, $routeParams, mvCachedKits) {
+    mvCachedKits.query().$promise.then(function(collection) {
+        collection.forEach(function(kit) {
+            if (kit._id === $routeParams.id) {
+                $scope.kit = kit;
+            }
+        })
+    })
 });
