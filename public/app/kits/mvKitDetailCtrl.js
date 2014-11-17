060a5ffffdbcaf96f4eea3d7f5c5022c40c52a18
jatjo <winterland78@hotmail.com>
2014-11-17 20:54:31 +0100

    Added fileupload client.

--- public/app/kits/mvKitDetailCtrl.js
+++ public/app/kits/mvKitDetailCtrl.js
@@ -1,9 +1,11 @@
-angular.module('app').controller('mvKitDetailCtrl', function($scope, $routeParams, mvCachedKits) {
+angular.module('app').controller('mvKitDetailCtrl', function($scope, $routeParams, mvCachedKits, FileUploader) {
     mvCachedKits.query().$promise.then(function(collection) {
         collection.forEach(function(kit) {
             if (kit._id === $routeParams.id) {
                 $scope.kit = kit;
             }
         })
-    })
+    });
+
+    $scope.uploader = new FileUploader();
 });
