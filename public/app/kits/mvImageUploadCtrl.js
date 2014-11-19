dd66d3b317a5aa2c99df09647b903863f81541dd
Jan Terje Johansen <jatjo@nets.eu>
2014-11-19 15:01:15 +0100

    Added image preview to image uploader. Started implementing image carousel.

--- public/app/kits/mvImageUploadCtrl.js
+++ public/app/kits/mvImageUploadCtrl.js
@@ -1,5 +1,21 @@
-angular.module('app').controller('mvImageUploadCtrl', function($scope, $modalInstance, FileUploader) {
-    $scope.uploader = new FileUploader();
+angular.module('app').controller('mvImageUploadCtrl', function($log, $scope, $modalInstance, FileUploader) {
+    var uploader = $scope.uploader = new FileUploader();
+
+    uploader.filters.push({
+      name: 'imageFilter',
+      fn: function(item /*{File|FileLikeObject}*/, options) {
+        var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+        return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+      }
+    });
+
+    uploader.onCompleteItem = function(fileItem, response, status, headers) {
+      $scope.slides.push({
+        image: fileItem,
+        text: 'Some text'
+      });
+      $log.info('onCompleteItem', fileItem, response, status, headers);
+    };
 
     $scope.submitForm = function() {
       $modalInstance.close();
--- public/app/kits/mvKitDetailCtrl.js
+++ public/app/kits/mvKitDetailCtrl.js
@@ -20,4 +20,17 @@ angular.module('app').controller('mvKitDetailCtrl', function($log, $scope, $rout
         });
     };
 
+    var slides = $scope.slides = [];
+    $scope.addSlide = function() {
+      var newWidth = 600 + slides.length + 1;
+      slides.push({
+        image: 'http://placekitten.com/' + newWidth + '/300',
+        text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+          ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+      });
+    };
+
+    /*for (var i=0; i<4; i++) {
+      $scope.addSlide();
+    };*/
 });
