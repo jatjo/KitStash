84684c4a10c6649487bd8c1c249944ea7c95a062
Jan Terje Johansen <jatjo@nets.eu>
2014-11-19 14:19:31 +0100

    Changed fileupload to modal window.

--- public/app/kits/mvImageUploadCtrl.js
+++ public/app/kits/mvImageUploadCtrl.js
@@ -1,3 +1,11 @@
-angular.module('app').controller('mvImageUploadCtrl', function($scope, FileUploader) {
+angular.module('app').controller('mvImageUploadCtrl', function($scope, $modalInstance, FileUploader) {
     $scope.uploader = new FileUploader();
+
+    $scope.submitForm = function() {
+      $modalInstance.close();
+    };
+
+    $scope.cancelForm = function() {
+      $modalInstance.dismiss();
+    };
 });
--- public/app/kits/mvKitDetailCtrl.js
+++ public/app/kits/mvKitDetailCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('mvKitDetailCtrl', function($scope, $routeParams, $modal, mvCachedKits) {
+angular.module('app').controller('mvKitDetailCtrl', function($log, $scope, $routeParams, $modal, mvCachedKits) {
     mvCachedKits.query().$promise.then(function(collection) {
         collection.forEach(function(kit) {
             if (kit._id === $routeParams.id) {
@@ -8,9 +8,16 @@ angular.module('app').controller('mvKitDetailCtrl', function($scope, $routeParam
     });
 
     $scope.uploadImage = function() {
-        $modal.open({
+        var modalInstance = $modal.open({
             templateUrl: '/partials/kits/image-upload',
             controller: 'mvImageUploadCtrl'
         });
+
+        modalInstance.result.then(function () {
+          $log.info('Modal closed at: ' + new Date());
+        }, function () {
+          $log.info('Modal dismissed at: ' + new Date());
+        });
     };
+
 });
